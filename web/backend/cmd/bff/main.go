b1c9c1dad2172e924c3cd0ecc60c0ca01ac7c677
Peixuan Li <peixuanli1233@foxmail.com>
2020-10-27 11:54:47 -0700

    Setup an example GraphQL Server; Added GraphQL Playground for debugging; Updated .gitignore for bff

--- web/backend/cmd/bff/main.go
+++ web/backend/cmd/bff/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	graphql "github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+)
+
+type query struct{}
+
+func (_ *query) Hello() string { return "Hello, world!" }
 
 func main() {
-	fmt.Println("Hello, world.")
+	// setup GraphQL schema and resolver
+	s := `
+		type Query {
+						hello: String!
+		}
+	`
+	schema := graphql.MustParseSchema(s, &query{})
+	http.Handle("/query", &relay.Handler{Schema: schema})
+
+	// register graphql playground handler
+	http.Handle("/", http.FileServer(http.Dir("./playground")))
+
+	// start web server
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
--- web/backend/go.mod
+++ web/backend/go.mod
@@ -1,3 +1,5 @@
 module web-backend
 
 go 1.15
+
+require github.com/graph-gophers/graphql-go v0.0.0-20201027172035-4c772c181653
